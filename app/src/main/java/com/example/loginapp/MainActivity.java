a40ad49b4db675ebd2d97a6fc3d24388ee52f0aa
joyetgeorge <earthayil9953>
2021-04-21 13:25:00 +0530

    v 1.1

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText userName = (EditText) findViewById(R.id.userName);
+        EditText password = (EditText) findViewById(R.id.password);
+        Log.i("login", "username : " +userName.getText().toString() +" | password : " +password.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
